8d80eee6ee24e58533dca59d5e051610cbae1001
shirish babbur <bbshirish@gmail.com>
2018-01-26 20:50:02 +0530

    Removed unnecessary libs and now data is in json dataframe

--- build.sbt
+++ build.sbt
@@ -5,12 +5,3 @@ libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
 libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0"
 libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka" % "1.6.2"
-
-mergeStrategy in assembly := {
-  case m if m.toLowerCase.endsWith("manifest.mf")          => MergeStrategy.discard
-  case m if m.toLowerCase.matches("meta-inf.*\\.sf$")      => MergeStrategy.discard
-  case "log4j.properties"                                  => MergeStrategy.discard
-  case m if m.toLowerCase.startsWith("meta-inf/services/") => MergeStrategy.filterDistinctLines
-  case "reference.conf"                                    => MergeStrategy.concat
-  case _                                                   => MergeStrategy.first
-}
--- src/main/scala/SparkReceiver.scala
+++ src/main/scala/SparkReceiver.scala
@@ -5,6 +5,7 @@ import org.apache.spark.streaming.Seconds
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 
 
 object SparkReceiver {
@@ -15,20 +16,18 @@ object SparkReceiver {
     val conf = new SparkConf().setAppName("SparkReceiver").setMaster("local[2]")
     val context = new StreamingContext(conf,Seconds(2))
     val kafkaParam = Map[String,String]("metadata.broker.list" -> broker)
-    val kafkaStream = KafkaUtils.createDirectStream[String,String,StringDecoder,StringDecoder](context,kafkaParam,topic.split(",").toSet)
-    println("Just before streaming--------------->")
-    //kafkaStream.print(50)
-    //val x = kafkaStream.map(_._2)
-    //x.print(100)
+    val kafkaStream = KafkaUtils.createDirectStream[String,String,StringDecoder,StringDecoder](context,kafkaParam,topic.split(",").toSet).map(_._2)
 
     kafkaStream.foreachRDD {
       rdd =>
-      val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
-      import spark.implicits._
-      val df = spark.createDataFrame(rdd)
-      df.createOrReplaceTempView("tweets")
-      val tweetData = spark.sql("select * from the tweets")
-      tweetData.show()
+      if(rdd.toLocalIterator.nonEmpty){
+        val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+        val sqlContext = new SQLContext(context.sparkContext)
+        import spark.implicits._
+        val tweet = sqlContext.jsonRDD(rdd)
+        val df = tweet.toDF()
+        df.withColumn("hashs", explode($"entities.hashtags")).filter($"hashs".isNotNull).select("hashs.text").show()
+      }
     }
     context.start()
     context.awaitTermination()
